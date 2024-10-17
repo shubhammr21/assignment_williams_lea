3b7140bd9a9761dc2a20a5517ef4e6dcceaff358
Shubham Maurya <shubhammr21@gmail.com>
2024-10-17 15:03:08 +0530

    feat(repository): add publisher repository methods
    - Add `get_all` method to fetch all publishers
    - Add `get_by_id` method to fetch publisher by ID
    
    Enhances the repository layer by introducing new methods to interact with the Publisher model. This allows for efficient retrieval and management of publisher data within the application.

--- src/apps/library/tests/test_repositories.py
+++ src/apps/library/tests/test_repositories.py
@@ -1,8 +1,9 @@
 import pytest
 
-from src.apps.library.tests.factories import AuthorFactory
-
 from ..repositories import AuthorRepository
+from ..repositories import PublisherRepository
+from .factories import AuthorFactory
+from .factories import PublisherFactory
 
 
 @pytest.mark.django_db
@@ -17,3 +18,15 @@ class TestAuthorRepository:
         repo = AuthorRepository()
         fetched_author = repo.get_by_id(author.id)
         assert fetched_author.name == "Author 1"
+
+
+@pytest.mark.django_db
+class TestPublisherRepository:
+    def test_get_all(self):
+        PublisherFactory(name="Publisher 1")
+        assert len(PublisherRepository.get_all()) == 1
+
+    def test_get_by_id(self):
+        publisher = PublisherFactory(name="Publisher 2")
+        fetched_publisher = PublisherRepository.get_by_id(publisher.id)
+        assert fetched_publisher.name == "Publisher 2"
