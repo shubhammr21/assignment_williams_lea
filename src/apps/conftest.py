8f3fc751857db878e32844f965e92422647cc3ab
Shubham Maurya <shubhammr21@gmail.com>
2024-10-18 09:33:19 +0530

    chore(tests): refactor conftest.py
    - Remove unused imports for User and UserFactory
    - Remove user fixture which is no longer needed
    
    This change cleans up the conftest.py file by removing unnecessary code related to User and UserFactory. This helps maintain a lean and focused codebase by eliminating unused dependencies and fixtures, making the codebase easier to maintain and understand.

--- src/apps/conftest.py
+++ src/apps/conftest.py
@@ -1,14 +1,6 @@
 import pytest
 
-from src.apps.users.models import User
-from src.apps.users.tests.factories import UserFactory
-
 
 @pytest.fixture(autouse=True)
 def _media_storage(settings, tmpdir) -> None:
     settings.MEDIA_ROOT = tmpdir.strpath
-
-
-@pytest.fixture
-def user(db) -> User:
-    return UserFactory()
